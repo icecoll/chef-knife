ea78b25b800edb8b4edf6d71981056526b718ded
sinalpha <hihahajun@gmail.com>
2016-01-21 10:19:12 +0800

    add ss and svpn recipe

---
+++ site-cookbooks/sinatra/recipes/ss.rb
@@ -0,0 +1,14 @@
+git '/root/ss' do
+  respository 'https://github.com/shadowsocks/shadowsocks-libev.git'
+  reference 'master'
+  action :sync
+end
+
+bash 'install ss' do
+  cwd '/root/ss'
+  code <<-EOH
+    touch done
+    ./configure && make && make install
+  EOH
+  not_if { ::File.exists? '/root/ss/done' }
+end
---
+++ site-cookbooks/sinatra/recipes/svpn.rb
@@ -0,0 +1,27 @@
+git '/root/svpn' do
+  respository 'https://github.com/lincank/ShadowVPN.git'
+  reference 'master'
+  action :sync
+end
+
+bash 'upgrade autoconf' do
+  cwd '/root'
+  code <<-EOH
+    wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+    tar xvfvz autoconf-2.69.tar.gz
+    cd autoconf-2.69
+    ./configure && make && make install
+  EOH
+  not_if { ::File.exists? '/root/autoconf-2.69.tar.gz' }
+end
+
+bash 'install svpn' do
+  cwd '/root/svpn'
+  code <<-EOH
+    git submodule update --init
+    touch done
+    ./autogen.sh
+    ./configure --enable-static --sysconfdir=/etc && make && make install
+  EOH
+  not_if { ::File.exists? '/root/svpn/done' }
+end
